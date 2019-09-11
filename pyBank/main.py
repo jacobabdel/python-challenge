1154c3962dd96e58ec7a33c1762cdb06386cc191
Jacob A <jacobaa@gmail.com>
2019-09-11 00:24:27 -0400

    new file:   pyBank/Resources/analysis.csv 	modified:   pyBank/main.py 	new file:   pyPoll/Resources/analysis.csv 	modified:   pyPoll/main.py

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,1 +1,76 @@
+import os
+import csv
 
+# Path to collect data from the Resources folder
+budget_data = os.path.join('..','pyBank', 'Resources', 'budget_data.csv')
+
+
+# Read in the CSV file
+with open(budget_data, 'r') as csvfile:
+    
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Skip header row
+    csv_header = next(csvreader, None)
+
+    # prepare lists
+    all_months = []
+    PandL_val = []
+    change_val = []
+
+    # Loop through the data
+    for row in csvreader:
+        
+        #insert csv values into lists
+        all_months.append(row[0])
+        PandL_val.append(int(row[1]))
+
+        #generate list of changes from month to month
+        if len(PandL_val) >= 2:
+          change_val.append((PandL_val[len(PandL_val)-1] - PandL_val[len(PandL_val)-2]))
+        
+    #calculate & format totals
+    total_months = len(all_months)
+    total_PandL = '${:,}'.format(sum(PandL_val))
+
+    #calculate & format average change
+    avg_change = sum(change_val) / total_months
+    avg_change = '${:,.2f}'.format(avg_change)
+    
+    #find & format max values from list
+    max_month = PandL_val.index(max(PandL_val))
+    max_month = all_months[max_month]
+    max_val = '${:,}'.format(max(PandL_val))
+
+    #find & format min values from list
+    min_month = PandL_val.index(min(PandL_val))
+    min_month = all_months[min_month]
+    min_val = '${:,}'.format(min(PandL_val))
+
+#print analysis
+print('Financial Analysis')
+print('------------------------------')
+print(f'Total Months:', total_months)
+print(f'Total:', total_PandL)
+print(f'Average Change:', avg_change)
+print(f'Greatest Increase in Profits:', max_month, max_val)
+print(f'Greatest Decrease in Profits:', min_month, min_val)
+
+#output analysis to file
+output_path = os.path.join('..','pyBank', 'Resources', 'analysis.csv')
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=' ')
+
+    # Write rows
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(['------------------------------'])
+    csvwriter.writerow(['Total Months', total_months])
+    csvwriter.writerow(['Total:', total_PandL])
+    csvwriter.writerow(['Average Change', avg_change])
+    csvwriter.writerow(['Greatest Increase in Profits', max_month, max_val])
+    csvwriter.writerow(['Greatest Decrease in Profits', min_month, min_val])
--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,1 +1,86 @@
+import os
+import csv
 
+# Path to collect data from the Resources folder
+election_data = os.path.join('..','pyPoll', 'Resources', 'election_data.csv')
+
+
+# Read in the CSV file
+with open(election_data, 'r') as csvfile:
+    
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Skip header row
+    csv_header = next(csvreader, None)
+
+    # prepare lists & variables
+    candidates = []
+    votes = []
+    can_can = []
+    can_perc = []
+    can_votes = []
+    total_votes = 0
+    vote_count = 0
+    percentage = 0
+
+    # Loop through the data to get list of all candidates & votes
+    for row in csvreader:
+        votes.append(total_votes)
+        candidates.append(row[2])
+        total_votes += 1
+    
+    #create set to get unique candidates, revert to list to allow iteration
+    candid_set = set(candidates)
+    can_can = list(candid_set)
+
+    #find vote count and percentage for each candidate
+    for candid in can_can:
+        #reset vote count for each candidate    
+        vote_count = 0
+
+        #tabulate number of votes for candidate
+        for vote in votes:
+            if candidates[vote] == candid:
+                vote_count += 1
+            
+        #calculate & format percentage
+        percentage = '{0:.00%}'.format(vote_count / total_votes) #* 100
+        
+        #add candidate percent and votes to lists
+        can_perc.append(percentage)
+        can_votes.append(vote_count)
+
+#determine winner
+winner = can_can[can_votes.index(max(can_votes))]
+        
+#print results
+print('Election Results')
+print('------------------------------')
+print(f'Total Votes: {vote_count}')
+print('------------------------------')
+for c in can_can:
+    print(f'{c}: {can_perc[can_can.index(c)]} ({can_votes[can_can.index(c)]})')
+print('------------------------------')
+print(f'Winner: {winner}')
+print('------------------------------')
+
+#output analysis to file
+output_path = os.path.join('..','pyPoll', 'Resources', 'analysis.csv')
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=' ')
+
+    # Write rows
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(['------------------------------'])
+    csvwriter.writerow(['Total Votes:', vote_count])
+    csvwriter.writerow(['------------------------------'])
+    for c in can_can:
+        csvwriter.writerow([c,':', can_perc[can_can.index(c)], '(',can_votes[can_can.index(c)],')'])
+    csvwriter.writerow(['------------------------------'])
+    csvwriter.writerow(['Winner:', winner])
+    csvwriter.writerow(['------------------------------'])
